1a6cb65aa3db8c7e0fd4066dfb0251821d825512
KoWming <1075151242@qq.com>
2024-12-22 13:09:30 +0800

    Update tieba_sign_httpmsg_callback.php

--- tieba_sign_httpmsg_callback.php
+++ tieba_sign_httpmsg_callback.php
@@ -2,16 +2,16 @@
 // 初始化回调函数 callback_init
 function callback_init() {
     // 设置一个定时任务（cron job）
-    // 第一个参数为任务名称 'sign_pushplus'
-    // 第二个参数为任务执行的文件路径 'plugins/tieba_sign_pushplus/send.php'
+    // 第一个参数为任务名称 'sign_httpmsg'
+    // 第二个参数为任务执行的文件路径 'plugins/tieba_sign_httpmsg/send.php'
     // 后面三个参数分别表示：分钟、小时和天的执行时间
     // 这里设置为每天的凌晨 0 点执行
-    cron::set('sign_pushplus', 'plugins/tieba_sign_pushplus/send.php', '0', '0', '0');
+    cron::set('sign_httpmsg', 'plugins/tieba_sign_httpmsg/send.php', '0', '0', '0');
 }
 
 // 移除回调函数 callback_remove
 function callback_remove() {
     // 删除之前设置的定时任务
-    cron::del('sign_pushplus');
+    cron::del('sign_httpmsg');
 }
 ?>
